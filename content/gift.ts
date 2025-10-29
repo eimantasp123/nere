7189e4de91be8655d5ff8d5b52c53a58650aeeab
Eimantas <eimantaspetrikas18@gmail.com>
2025-10-29 09:08:37 +0100

    last

--- content/gift.ts
+++ content/gift.ts
@@ -30,12 +30,12 @@ export const options = [
     price: "110€",
   },
   { label: "Ritualas vyrams", duration: "70 min", price: "60€" },
-  { label: "Plaukų SPA (trumpi plaukai)", duration: "120 min", price: "50€" },
+  { label: "Plaukų SPA (trumpi plaukai)", duration: "90 min", price: "50€" },
   {
     label: "Plaukų SPA (vidutinio ilgio plaukai)",
-    duration: "135 min",
+    duration: "105 min",
     price: "70€",
   },
-  { label: "Plaukų SPA (ilgi plaukai)", duration: "150 min", price: "80€" },
+  { label: "Plaukų SPA (ilgi plaukai)", duration: "120 min", price: "80€" },
   { label: "Dovanų kuponas už pasirinktą sumą" },
 ];
