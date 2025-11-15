4cd83065b85a68d1fac5951a24568a074b891f2e
Eimantas <eimantaspetrikas18@gmail.com>
2025-11-15 23:47:42 +0100

    last

--- app/api/gift/route.ts
+++ app/api/gift/route.ts
@@ -1,11 +1,10 @@
 import { transporter } from "../_utils/transporter";
 
 export async function POST(req: Request) {
-  const { fullName, email, recipient, message, token, ritual, voucherAmount } =
-    await req.json();
+  const { token, ...data } = await req.json();
 
   // Validate
-  if (!fullName || !email || !ritual || !recipient || !token) {
+  if (!data || !token) {
     return new Response("Missing fields", { status: 400 });
   }
 
@@ -29,15 +28,17 @@ export async function POST(req: Request) {
     await transporter.sendMail({
       from: `"Nere.lt" <${process.env.EMAIL_USER}>`,
       to: "labas@nere.lt",
-      replyTo: email,
+      replyTo: data.email,
       subject: `Dovanų kupono užklausa`,
-      text: message,
-      html: `<p><strong>Žinutė nuo:</strong> ${fullName}<br/>
-            <strong>El. Paštas:</strong> ${email}<br/>
-            <strong>Kupono gavėjo vardas:</strong> ${recipient}<br/>
-            <strong>Ritualas:</strong> ${ritual}<br/>
-            ${voucherAmount ? `<strong>Kupono suma:</strong> ${voucherAmount}<br/>` : ""}
-            ${message ? `<strong>Žinutė:</strong> ${message}</p>` : ""}`,
+      text: data.message,
+      html: `<p><strong>Žinutė nuo:</strong> ${data.fullName}<br/>
+            <strong>Telefono numeris:</strong> ${data.phoneNumber}<br/>
+            <strong>El. Paštas:</strong> ${data.email}<br/>
+            <strong>Kupono gavėjo vardas:</strong> ${data.recipient}<br/>
+            <strong>Ritualas:</strong> ${data.ritual}<br/>
+            ${data.voucherAmount ? `<strong>Kupono suma:</strong> ${data.voucherAmount}<br/>` : ""}
+            <strong>Kupono tipas:</strong> ${data.voucherType}<br/>
+            ${data.message ? `<strong>Žinutė:</strong> ${data.message}</p>` : ""}`,
     });
 
     return new Response("El. laiškas išssiųstas sėkmingai.", { status: 200 });
